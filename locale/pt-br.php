d085cf475e021834c954bb36343cd1289f5af750
klebereduardo <kleber_cioccari@yahoo.com.br>
2013-05-16 16:49:40 -0300

    Add pt-br language

---
+++ locale/pt-br.php
@@ -0,0 +1,22 @@
+<?php
+    return array(
+        'monthNames'=>array(
+            'Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'
+        ),
+        'monthNamesShort'=>array(
+            'Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'
+        ),
+        'firstDay'=>1,
+        'dayNames'=>array(
+            'Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'
+        ),
+        'dayNamesShort'=>array(
+            'Dom','Seg','Ter','Qua','Qui','Sex','Sab'
+        ),
+        'buttonText'=>array(
+            'today'=>'hoje',
+            'month'=>'mês',
+            'week'=>'semana',
+            'day'=>'dia'
+        ),
+    );
