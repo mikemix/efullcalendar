ac82d06f78f6e1e741efd0864fa8c0eda40a324b
Herbert Maschke <thyseus@gmail.com>
2013-12-21 12:34:16 +0100

    added german localization file

---
+++ locale/de.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'monthNames'=>array(
+        'Januar','Februar','März','April','Mai','Juni',
+        'Juli','August','September','Oktober','November','Dezember'
+    ),
+    'monthNamesShort'=>array(
+        'Jan','Feb','Mär','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'
+    ),
+    'firstDay'=>0,
+    'dayNames'=>array(
+        'Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag','Sonntag'
+    ),
+    'dayNamesShort'=>array(
+        'Mon','Die','Mit','Don','Fre','Sam','Son'
+    ),
+    'buttonText'=>array(
+        'today'=>'Heute',
+        'month'=>'monat',
+        'week'=>'Woche',
+        'day'=>'Tag'
+    ),
+);
