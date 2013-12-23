88f871037dd78b14699f34b67fbd150bd566c750
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-12-23 14:20:33 +0100

    locale: add french translations

---
+++ locale/fr.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+	'monthNames'=>array(
+		'Janvier','Février','Mars','Avril','Mai','Juin',
+		'Juillet','Août','Septembre','Octobre','Novembre','Décembre'
+	),
+	'monthNamesShort'=>array(
+		'jan.','fév.','mars','avr.','mai','juin','juil.','août','sep.','oct.','nov.','déc.'
+	),
+	'dayNames'=>array(
+		'Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'
+	),
+	'dayNamesShort'=>array(
+		'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'
+	),
+	'buttonText'=>array(
+		'today'=>'Ce jour',
+		'month'=>'Mois',
+		'week'=>'Semaine',
+		'day'=>'Jour'
+	),
+);
